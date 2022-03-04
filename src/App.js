b81e5deb0d8c89efeaa23362e65eb6e710760f67
Sal Castoro <salcastoro@gmail.com>
2022-03-04 15:01:02 -0500

    Add dice styling, functionality to control color

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import React from "react";
-import "./App.css";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
+import "./App.css";
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+  function allNewDice() {
+    const diceArr = [];
+    for (let i = 0; i < 10; i++) {
+      diceArr.push({
+        value: Math.floor(Math.random() * 6 + 1),
+        isHeld: true,
+        id: nanoid(),
+      });
+    }
+    return diceArr;
+  }
+  function rollDice() {
+    setDice(allNewDice());
+  }
+  function toggleIsHeld(currentId) {
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        die.id === currentId ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+  const diceElements = dice.map((die, index) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      held={die.isHeld}
+      handleHeld={toggleIsHeld}
+      id={die.id}
+    />
+  ));
   return (
     <main>
-      <section className="die-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+      <section className="game-text">
+        <h1 className="game-text-heading">Tenzies</h1>
+        <p className="game-text-para">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
       </section>
+      <section className="die-container">{diceElements}</section>
+      <button className="roll-dice-btn" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Die(props) {
-  return <div className="die">{props.value}</div>;
+  return (
+    <div
+      className={"die " + (props.held ? "held" : "")}
+      onClick={() => props.handleHeld(props.id)}
+    >
+      {props.value}
+    </div>
+  );
 }
 
 export default Die;
